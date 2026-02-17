75d2b97490eaf78456d2504ee8388c8f3d2bdfe8
itoryon <226760486+itoryon@users.noreply.github.com>
2026-02-17 16:34:23 +0500

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -4,9 +4,12 @@ G='\e[32m'
 B='\e[34m'
 N='\e[0m'
 
+echo -e "${G}[*] Обновление списков пакетов...${N}"
+pkg update -y
+
 echo -e "${G}[*] Подготовка системы meoww (Web Edition)...${N}"
 
-# Список нужных пакетов (убрали ncurses, добавили openssl)
+# Список нужных пакетов
 PKGS=("clang" "libcurl" "openssl" "termux-api" "nlohmann-json")
 
 for pkg in "${PKGS[@]}"; do
@@ -16,10 +19,10 @@ for pkg in "${PKGS[@]}"; do
     fi
 done
 
-# Скачиваем httplib.h, если его нет (нужен для компиляции)
+# Скачиваем httplib.h, если его нет
 if [ ! -f "httplib.h" ]; then
     echo -e "${B}[*] Скачивание сетевого модуля httplib...${N}"
     curl -L "https://raw.githubusercontent.com/yhirose/cpp-httplib/master/httplib.h" -o "httplib.h"
 fi
 
-echo -e "${G}[OK] Установка зависимостей завершена!${N}"
+echo -e "${G}[OK] Подготовка завершена! Теперь запустите скрипт установки чата.${N}"
